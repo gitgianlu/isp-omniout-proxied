af879a4cd1a2db8aac0d3abca603033128d6bf6e
GC <kamikaz@tiscali.it>
2021-12-07 15:08:42 +0100

    build

---
+++ isp-omniout-proxied/build/precache-manifest.1e33b18ebf8b5a996b99decc1664b678.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cd75fbc2407f7378b1b206dbaf9d34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8395a627434ff6fbae12",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "8439f72bd9eecebc9d8b",
+    "url": "/static/css/main.bba74cd9.chunk.css"
+  },
+  {
+    "revision": "8395a627434ff6fbae12",
+    "url": "/static/js/2.c8b0c968.chunk.js"
+  },
+  {
+    "revision": "97dbe745422fb8b997ae12705f080bcb",
+    "url": "/static/js/2.c8b0c968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8439f72bd9eecebc9d8b",
+    "url": "/static/js/main.96c53e51.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "859215c087cfcba57ec315b84aaf1815",
+    "url": "/static/media/home.859215c0.jpg"
+  },
+  {
+    "revision": "207d77810305e0553eef9b3d30ff992e",
+    "url": "/static/media/insurance.207d7781.jpg"
+  },
+  {
+    "revision": "204373a9f8ff4c9dbfe1e62b7d74a03a",
+    "url": "/static/media/life.204373a9.jpg"
+  },
+  {
+    "revision": "33fc20553aa3dbfe183a077d58bcd348",
+    "url": "/static/media/location.33fc2055.png"
+  }
+]);
